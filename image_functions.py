e8b7546e34bc3d88500eb9270cc6e02f26041526
Wimbe <WIMBERLY.remington@gmail.com>
2024-03-16 11:24:41 -0400

    init api commit

--- image_functions.py
+++ image_functions.py
@@ -53,11 +53,23 @@ def merge_images_with_text(image1_path, image2_path, text="TEXT", font_path="Fjo
     return final_image
 
 
+# if __name__ == "__main__":
+#     image1_path = "test_images/prior_images/card-bg-7.jpg"
+#     image2_path = "test_images/prior_images/golden-monke.png"
+#
+#     merged_image = merge_images_with_text(image1_path, image2_path)
+#     merged_image.save("merged_image_with_text.png")
+#     print("Merged image with text saved as 'merged_image_with_text.png'")
+
+
 if __name__ == "__main__":
     image1_path = "test_images/prior_images/card-bg-7.jpg"
     image2_path = "test_images/prior_images/golden-monke.png"
+    text = "TEXT"
+    font_path = "Fjord_Regular.ttf"
+    font_size = 40
+    text_color = (1, 5, 28)
 
-    merged_image = merge_images_with_text(image1_path, image2_path)
+    merged_image = merge_images_with_text(image1_path, image2_path, text, font_path, font_size, text_color)
     merged_image.save("merged_image_with_text.png")
     print("Merged image with text saved as 'merged_image_with_text.png'")
-
