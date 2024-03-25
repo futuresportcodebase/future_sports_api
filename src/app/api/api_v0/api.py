bdb6ec2a3f46be961e6be2ce7b5a8fcb649ca8c9
Wimbe <WIMBERLY.remington@gmail.com>
2024-03-25 09:29:41 -0400

    containerized

--- src/app/api/api_v0/api.py
+++ src/app/api/api_v0/api.py
@@ -130,42 +130,44 @@ def upload_to_s3(image_data: bytes, filename: str, bucket="futuresportsimages"):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+# from pydantic import BaseModel, Field, HttpUrl
+
+
+class MergeImageInput(BaseModel):
+    background: str = "https://nftstorage.link/ipfs/bafybeibfnwmpyd73f737jgr256wxr3wv35hlstvvmjyi4tf7ns3zzzeiue"
+    pfp: str = "https://nftstorage.link/ipfs/bafkreigymxzyva7uwtogbwlmxjcwonq2tyihd3fixt3t6y3nqjvkc5xdfa"
+    header_text: str = "Header"
+    text: str = "TEXT"
+    font_path: str = "app/api/api_v0/assets/Minercraftory.ttf"
+    font_size: int = 50
+    header_font_size: int = 50
+    text_color: Tuple[int, int, int] = (250, 221, 177)
+    header_color: Tuple[int, int, int] = (250, 221, 177)
+    text_vertical_position: int = 1100
+    header_vertical_position: int = 140
+
 
 @router.post("/merge_and_upload")
-async def merge_and_upload(background: str = Query(
-    "s3://futuresportsimages/images/bafybeibfnwmpyd73f737jgr256wxr3wv35hlstvvmjyi4tf7ns3zzzeiue"),
-                           pfp: str = Query(
-                               "s3://futuresportsimages/images/bafkreigymxzyva7uwtogbwlmxjcwonq2tyihd3fixt3t6y3nqjvkc5xdfa"),
-                           header_text: str = Query("Header", description="Header Text"),
-                           text: str = Query("TEXT", description="Main Text"),
-                           font_path: str = Query("app/api/api_v0/assets/Minercraftory.ttf", description="Font Path"),
-                           font_size: int = Query(50, description="Font Size for Main Text"),
-                           header_font_size: int = Query(50, description="Font Size for Header Text"),
-                           text_color: str = Query("(250, 221, 177)", description="Color of Main Text (R, G, B)"),
-                           header_color: str = Query("(250, 221, 177)", description="Color of Header Text (R, G, B)"),
-                           text_vertical_position: int = Query(1100, description="Vertical Position of Main Text"),
-                           header_vertical_position: int = Query(140, description="Vertical Position of Header Text")):
+async def merge_and_upload(input_data: MergeImageInput):
     try:
-        # Convert string tuples to actual tuples for colors
-        text_color_tuple = tuple(map(int, text_color.strip("()").split(", ")))
-        header_color_tuple = tuple(map(int, header_color.strip("()").split(", ")))
-
-
         # Check if image paths are URLs and handle accordingly
-        # Ensure download_image_to_s3 function is implemented to handle this logic
+        if input_data.pfp.startswith("http://") or input_data.pfp.startswith("https://"):
+            # Download image from URL and upload to S3
+            pfp = download_image_to_s3(input_data.pfp)
+        else:
+            pfp = input_data.pfp  # Assume it's already an S3 path
 
-        if pfp.startswith("http://") or pfp.startswith("https://"):
+        if input_data.background.startswith("http://") or input_data.background.startswith("https://"):
             # Download image from URL and upload to S3
-            pfp = download_image_to_s3(pfp)
-        if background.startswith("http://") or background.startswith("https://"):
-            # Download image from URL and upload to S3
-            background = download_image_to_s3(background)
+            background = download_image_to_s3(input_data.background)
+        else:
+            background = input_data.background  # Assume it's already an S3 path
 
         # Merge images with additional options
-        merged_image_bytes = merge_images(background, pfp, header_text, text,
-                                          font_path, font_size, header_font_size,
-                                          text_color_tuple, header_color_tuple,
-                                          text_vertical_position, header_vertical_position)
+        merged_image_bytes = merge_images(background, pfp, input_data.header_text, input_data.text,
+                                          input_data.font_path, input_data.font_size, input_data.header_font_size,
+                                          input_data.text_color, input_data.header_color,
+                                          input_data.text_vertical_position, input_data.header_vertical_position)
 
         # Upload merged image to S3 and generate a URL
         filename = datetime.now().strftime(
@@ -177,6 +179,7 @@ async def merge_and_upload(background: str = Query(
         raise e
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
 
 
 def download_image_to_s3(image_url: str, bucket="futuresportsimages"):
@@ -198,21 +201,9 @@ def download_image_to_s3(image_url: str, bucket="futuresportsimages"):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-class MergeImageViewInput(BaseModel):
-    background: str = "https://nftstorage.link/ipfs/bafybeibfnwmpyd73f737jgr256wxr3wv35hlstvvmjyi4tf7ns3zzzeiue"
-    pfp: str = "https://nftstorage.link/ipfs/bafkreigymxzyva7uwtogbwlmxjcwonq2tyihd3fixt3t6y3nqjvkc5xdfa"
-    header_text: str = "Header"
-    text: str = "TEXT"
-    font_path: str = "app/api/api_v0/assets/Minercraftory.ttf"
-    font_size: int = 50
-    header_font_size: int = 50
-    text_color: Tuple[int, int, int] = (250, 221, 177)
-    header_color: Tuple[int, int, int] = (250, 221, 177)
-    text_vertical_position: int = 1100
-    header_vertical_position: int = 140
 
 @router.post("/merge_and_view")
-async def merge_and_view(input_data: MergeImageViewInput):
+async def merge_and_view(input_data: MergeImageInput):
     try:
         if input_data.background.startswith("http://") or input_data.background.startswith("https://"):
             # Download image from URL and upload to S3
